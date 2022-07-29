e312dda0d837f939c2e8994a49ea653eeb5fea47
Kiprich <davidkipkemoi30@gmail.com>
2022-07-29 11:37:11 +0300

    init

---
+++ resources/js/components/addItemForm.vue
@@ -0,0 +1,49 @@
+<template>
+<div class ="addItem">
+    <input type ="text" v-model="item.name"/>
+    <font-awesome-icon
+        icon ="plus-square"
+        :class="[item.name ? 'active' :'inactive', 'plus']" />
+</div>
+</template>
+<script>
+export default{
+    data: function (){
+        return {
+        item:{
+                name:""
+            }
+        }
+    }
+
+}
+
+
+</script>
+<style scoped>
+.addItem{
+    display:flex;
+    justify-content: center;
+    align-items: center;
+
+}
+.input{
+    background: white;
+    border:0px ;
+    outline: none;
+    padding: 5px ;
+    margin-right:10px ;
+    width: 100%;
+
+}
+.plus{
+    font-size: 20px;
+}
+.active{
+    color: rgb(226, 226, 12);
+}
+.inactive{
+    color: rgb(255, 0, 0);
+}
+
+</style>
