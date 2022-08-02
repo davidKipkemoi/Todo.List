21911e7c0531216b5c5f662d919b0f0851faca1d
Kiprich <davidkipkemoi30@gmail.com>
2022-08-03 01:28:26 +0300

    Morning

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,17 +4,11 @@ import { createApp} from 'vue'
 import TodoList from './components/TodoList.vue'
 
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faSquarePlus, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { fas} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-library.add(faSquarePlus, faTrash)
-createApp(TodoList).component ('font-awesome-icon',FontAwesomeIcon)
-s
+library.add(fas)
 
-
-const app = createApp({
-
-})
-
-app.component('app-component',TodoList)
-app.mount('#app')
+createApp(TodoList)
+.component('fa', FontAwesomeIcon)
+.mount('#app')
