fc29a83db4f528cf4783f2d2a8cad380c004491e
Kiprich <davidkipkemoi30@gmail.com>
2022-08-20 13:46:57 +0300

    Changes

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,8 @@
 require('./bootstrap');
 
 import {createApp} from 'vue'
+import { createVuetify } from 'vuetify'
+
 import TodoList from './components/TodoList'
 import addItemForm from './components/addItemForm.vue'
 import ListView from './components/ListView.vue'
@@ -15,11 +17,9 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 library.add(faPlusSquare, faTrash)
 createApp(TodoList,addItemForm,ListItem,ListView).component('font-awesome-icon',FontAwesomeIcon)
-
-const app = createApp({
-
-
-})
+const vuetify = createVuetify()
+const app = createApp({})
+app.use(vuetify)
 
 app.component('app-component',TodoList)
 app.mount('#app')
